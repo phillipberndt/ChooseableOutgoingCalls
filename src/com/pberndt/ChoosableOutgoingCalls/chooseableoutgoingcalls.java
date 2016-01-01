1a35d292cbad9a9fd65858d38f34b4250dc6997d
Phillip Berndt <phillip.berndt@googlemail.com>
2016-01-01 23:00:23 +0100

    Experimental Lollipop support (See #4 and #3)

--- src/com/pberndt/ChoosableOutgoingCalls/chooseableoutgoingcalls.java
+++ src/com/pberndt/ChoosableOutgoingCalls/chooseableoutgoingcalls.java
@@ -7,6 +7,7 @@ import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import android.net.Uri;
+import android.os.Build;
 
 
 public class chooseableoutgoingcalls implements IXposedHookLoadPackage {
@@ -14,20 +15,32 @@ public class chooseableoutgoingcalls implements IXposedHookLoadPackage {
 		if (!lpparam.packageName.equals("com.android.contacts")) {			
 			return;
 		}
-		
-		findAndHookMethod("com.android.contacts.common.CallUtil", lpparam.classLoader, "getCallIntent", Uri.class, String.class, new XC_MethodReplacement() {
-			@Override
-			protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-				final Intent intent = new Intent("android.intent.action.VIEW", (Uri)param.args[0]);
-				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				String callOrigin = (String)param.args[1];
-				if (callOrigin != null) {
-					// From com.android.phone.common.PhoneConstants
-					intent.putExtra("com.android.phone.CALL_ORIGIN", callOrigin);
+
+		if(Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+			findAndHookMethod("com.android.contacts.common.CallUtil", lpparam.classLoader, "getCallIntent", Uri.class, new XC_MethodReplacement() {
+				@Override
+				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+					final Intent intent = new Intent("android.intent.action.VIEW", (Uri) param.args[0]);
+					intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+					return intent;
 				}
-				return intent;
-			}
-		});
+			});
+		}
+		else {
+			findAndHookMethod("com.android.contacts.common.CallUtil", lpparam.classLoader, "getCallIntent", Uri.class, String.class, new XC_MethodReplacement() {
+				@Override
+				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+					final Intent intent = new Intent("android.intent.action.VIEW", (Uri) param.args[0]);
+					intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+					String callOrigin = (String) param.args[1];
+					if (callOrigin != null) {
+						// From com.android.phone.common.PhoneConstants
+						intent.putExtra("com.android.phone.CALL_ORIGIN", callOrigin);
+					}
+					return intent;
+				}
+			});
+		}
 	}
 
 }
