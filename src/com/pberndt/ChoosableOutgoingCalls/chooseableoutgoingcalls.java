2904d09922f0fb41fe3f0b3a1a3e41fa2e04df6f
Phillip Berndt <phillip.berndt@googlemail.com>
2016-01-02 21:03:05 +0100

    Lollipop support, next attempt (bugs #4)

--- src/com/pberndt/ChoosableOutgoingCalls/chooseableoutgoingcalls.java
+++ src/com/pberndt/ChoosableOutgoingCalls/chooseableoutgoingcalls.java
@@ -8,19 +8,36 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import android.net.Uri;
 import android.os.Build;
-
+import android.telecom.PhoneAccountHandle;
+import android.telecom.PhoneAccount;
+import android.telecom.PhoneAccountHandle;
+import android.telecom.TelecomManager;
+import android.telecom.VideoProfile;
 
 public class chooseableoutgoingcalls implements IXposedHookLoadPackage {
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-		if (!lpparam.packageName.equals("com.android.contacts")) {			
+		if (!lpparam.packageName.equals("com.android.contacts") && !lpparam.packageName.equals("com.google.android.contacts")) {
 			return;
 		}
 
 		if(Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
-			findAndHookMethod("com.android.contacts.common.CallUtil", lpparam.classLoader, "getCallIntent", Uri.class, new XC_MethodReplacement() {
+			// Lollipop only, Marshmallow doesn't need this
+			findAndHookMethod("com.android.contacts.common.CallUtil", lpparam.classLoader, "getCallIntent", Uri.class, String.class, PhoneAccountHandle.class, int.class, new XC_MethodReplacement() {
 				@Override
 				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-					final Intent intent = new Intent("android.intent.action.VIEW", (Uri) param.args[0]);
+					Uri uri = (Uri)param.args[0];
+					String callOrigin = (String)param.args[1];
+					PhoneAccountHandle accountHandle = (PhoneAccountHandle)param.args[2];
+					int videoState = (int)param.args[3];
+
+					final Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+					intent.putExtra(TelecomManager.EXTRA_START_CALL_WITH_VIDEO_STATE, videoState);
+					if (callOrigin != null) {
+						intent.putExtra("com.android.phone.CALL_ORIGIN", callOrigin);
+					}
+					if (accountHandle != null) {
+						intent.putExtra(TelecomManager.EXTRA_PHONE_ACCOUNT_HANDLE, accountHandle);
+					}
 					intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 					return intent;
 				}
